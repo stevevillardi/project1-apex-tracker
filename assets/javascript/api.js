2fcc8ad8285c5e549f1b6e92873763189a6b4fd6
Steve Villardi <42367049+stevevillardi@users.noreply.github.com>
2019-10-30 21:34:36 -0400

    Modified Pokemon Search

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    function apiSearch() {
-        var pokemonName = "treecko"
+    function apiSearch(pokemonName) {
         var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + pokemonName + "/";
 
         $.ajax({
@@ -8,24 +7,35 @@ $(document).ready(function() {
             method: "GET"
         }).done(function(response) {
             results = response;
-            name = results.name;
-            sprite = results.sprites.front_default;
-            number = results.id;
-            type = results.types[0].type.name;
-            baseSPD = "Base Speed: " + results.stats[0].base_stat + " ";
-            baseSPDEF = "Base Special-Defense: " + results.stats[1].base_stat + " ";
-            baseSPATK = "Base Special-Attack: " + results.stats[2].base_stat + " ";
-            baseDEF = "Base Defense: " + results.stats[3].base_stat + " ";
-            baseATK = "Base Attack: " + results.stats[4].base_stat + " ";
-            baseHP = "Base Health Points: " + results.stats[5].base_stat + " ";
-            baseStats = baseSPD + baseSPDEF + baseSPATK + baseDEF + baseATK + baseHP;
 
-            console.log(name);
-            console.log(sprite);
-            console.log(number);
-            console.log(type);
-            console.log(baseStats);
+            let pokemonObj = {
+                name : results.name,
+                sprite : results.sprites.front_default,
+                number : results.id,
+                type : results.types[0].type.name,
+                baseSPD : results.stats[0].base_stat,
+                baseSPDEF : results.stats[1].base_stat,
+                baseSPATK : results.stats[2].base_stat,
+                baseDEF : results.stats[3].base_stat,
+                baseATK : results.stats[4].base_stat,
+                baseHP : results.stats[5].base_stat
+            };
+
+            console.log(pokemonObj)
+            return pokemonObj
         });
     };
-    apiSearch();
+
+    function displaySearch(pokemon){
+        $("#search-container").empty();
+        let title =$("<h1>").text(`${pokemon.name}`)
+
+    }
+
+    $("#submit").on("click", function(e){
+        e.preventDefault();
+        let searchTerm = $("#input-box").val();
+        let pokemon = apiSearch(searchTerm);
+        displaySearch(pokemon);
+    });
 })
